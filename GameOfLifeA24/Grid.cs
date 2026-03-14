b0c96da46b4cb5106cc1a3b8f75d5c3e8ee36580
Nathan Goyer <nathangoyer56@gmail.com>
2026-03-14 18:16:03 +0100

    Grid implementation first part

--- GameOfLifeA24/Grid.cs
+++ GameOfLifeA24/Grid.cs
@@ -7,4 +7,21 @@ namespace GameOfLifeA24;
 internal sealed class Grid
 {
     //Game environment where you will find a representation of each cells.
+    private int rows;
+    private int columns;
+
+    private Cell[,] cells;
+
+    public Grid(int rows, int columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+
+        cells = new Cell[rows, columns];
+    }
+
+    public Cell GetCell(int x, int y)
+    {
+        return cells[x, y];
+    }
 }
